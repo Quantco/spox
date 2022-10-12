1cbe03c8d7ed2ec531e9e4eed1e3a3dcedd1b7c5
Christian Bourjau <christian.bourjau@quantco.com>
2022-10-12 14:00:53 +0000

    Enable the conversion of non-hashable object

--- src/steelix/converter.py
+++ src/steelix/converter.py
@@ -9,14 +9,24 @@ converters: Dict[Any, Any] = {}
 def convert(what):
     """
     Implements a simple conversion protocol. Given an object, attempts:
-    1. Object conversion. If the object is in the ``converters`` dictionary, the associated value is returned.
-    2. Type conversion. If the type of the object is in the ``converters`` dictionary, the associated value
-        is called with ``what`` as the only parameter (the value decorates ``what``).
+
+    1. Object conversion. If the object is in the ``converters``
+       dictionary, the associated value is returned.
+    2. Type conversion. If the type of the object is in the
+       ``converters`` dictionary, the associated value is called with
+       ``what`` as the only parameter (the value decorates ``what``).
+
     Otherwise, ``KeyError`` is raised.
     """
-    if what in converters:  # Object conversion
-        return converters[what]
-    elif type(what) in converters:  # Type conversion
+    try:
+        # Object conversion (e.g. functions)
+        if what in converters:
+            return converters[what]
+    except TypeError:
+        # what may not be hashable
+        pass
+    # Type conversion
+    if type(what) in converters:
         return converters[type(what)](what)
     raise KeyError(
         f"No object or type converter for {what}. Did you forget to add one to the converters dictionary?"
