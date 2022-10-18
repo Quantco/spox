cd8ccd04425f8048906265941c07d266aca76b81
Jakub Bachurski <kbachurski@gmail.com>
2022-10-18 23:41:43 +0100

    Fix the inference test

--- tests/type_inference/test_one_hot_encoder.py
+++ tests/type_inference/test_one_hot_encoder.py
@@ -9,8 +9,8 @@ def test_one_hot_encoder_inference():
     (str_cats, int_cats) = steelix.arguments(str_cats=steelix.Tensor(np.str_, ("N", 5)), int_cats=steelix.Tensor(np.str_, ("N", 5)))
     str_encoded = op_ml.one_hot_encoder(str_cats, cats_strings=["a", "b", "c"])
     int_encoded = op_ml.one_hot_encoder(int_cats, cats_int64s=[0, 1, 2, 3])
-    assert str_encoded.unwrap_tensor() == ("N", 5, 3)
-    assert int_encoded.unwrap_tensor() == ("N", 5, 4)
+    assert str_encoded.unwrap_tensor().shape.to_simple() == ("N", 5, 3)
+    assert int_encoded.unwrap_tensor().shape.to_simple() == ("N", 5, 4)
 
 
 def test_one_hot_encoder_runs(onnx_helper):
