3be7df8fe8dff1b9666dc7c3d160a5405b9d5e5e
Jakub Bachurski <kbachurski@gmail.com>
2022-10-18 23:48:40 +0100

    Don't unwrap before type comparison

--- tests/type_inference/test_one_hot_encoder.py
+++ tests/type_inference/test_one_hot_encoder.py
@@ -9,8 +9,8 @@ def test_one_hot_encoder_inference():
     (str_cats, int_cats) = arguments(str_cats=Tensor(np.str_, ("N", 5)), int_cats=Tensor(np.int64, ("N", 5)))
     str_encoded = op_ml.one_hot_encoder(str_cats, cats_strings=["a", "b", "c"])
     int_encoded = op_ml.one_hot_encoder(int_cats, cats_int64s=[0, 1, 2, 3])
-    assert str_encoded.unwrap_tensor() == Tensor(np.float32, ("N", 5, 3))
-    assert int_encoded.unwrap_tensor() == Tensor(np.float32, ("N", 5, 4))
+    assert str_encoded.type == Tensor(np.float32, ("N", 5, 3))
+    assert int_encoded.type == Tensor(np.float32, ("N", 5, 4))
 
 
 def test_one_hot_encoder_runs(onnx_helper):
