e41352dce168cd8308a1e976d7e02b3dc911aed6
Jakub Bachurski <kbachurski@gmail.com>
2022-10-18 23:43:41 +0100

    Check the entire Tensor type in test

--- tests/type_inference/test_one_hot_encoder.py
+++ tests/type_inference/test_one_hot_encoder.py
@@ -1,23 +1,23 @@
 import numpy as np
 
-import steelix
+from steelix import arguments, results, Tensor
 import steelix.opset.ai.onnx.ml.v3 as op_ml
 
 
 def test_one_hot_encoder_inference():
     """Test type and shape inference for ``OneHotEncoder``."""
-    (str_cats, int_cats) = steelix.arguments(str_cats=steelix.Tensor(np.str_, ("N", 5)), int_cats=steelix.Tensor(np.str_, ("N", 5)))
+    (str_cats, int_cats) = arguments(str_cats=Tensor(np.str_, ("N", 5)), int_cats=Tensor(np.int64, ("N", 5)))
     str_encoded = op_ml.one_hot_encoder(str_cats, cats_strings=["a", "b", "c"])
     int_encoded = op_ml.one_hot_encoder(int_cats, cats_int64s=[0, 1, 2, 3])
-    assert str_encoded.unwrap_tensor().shape.to_simple() == ("N", 5, 3)
-    assert int_encoded.unwrap_tensor().shape.to_simple() == ("N", 5, 4)
+    assert str_encoded.unwrap_tensor() == Tensor(np.float32, ("N", 5, 3))
+    assert int_encoded.unwrap_tensor() == Tensor(np.float32, ("N", 5, 4))
 
 
 def test_one_hot_encoder_runs(onnx_helper):
     """Test custom type and shape inference and correct runtime result of the ``OneHotEncoder``."""
-    (str_cats,) = steelix.arguments(str_cats=steelix.Tensor(np.str_, ("N",)))
+    (str_cats,) = arguments(str_cats=Tensor(np.str_, ("N",)))
     encoded = op_ml.one_hot_encoder(str_cats, cats_strings=np.array(["a", "b", "c"]))
-    graph = steelix.results(encoded=encoded)
+    graph = results(encoded=encoded)
     onnx_helper.assert_close(
         onnx_helper.run(graph, "encoded", str_cats=["a", "b", "c", "z"]),
         np.array(
