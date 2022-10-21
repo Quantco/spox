2dd13e5199870465c2d6df0fde259be859f03fb5
Jakub Bachurski <kbachurski@gmail.com>
2022-10-21 16:19:30 +0100

    Fix typo in concrete check

--- src/steelix/type_system.py
+++ src/steelix/type_system.py
@@ -222,7 +222,7 @@ class Tensor(Type):
         )
 
     def assert_concrete(self, *, _traceback_name: str = "?"):
-        if self.shape is None:
+        if not self.shape:
             raise ValueError(
                 f"Tensor {self} does not specify the shape -- in {_traceback_name}."
             )
