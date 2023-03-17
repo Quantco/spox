d184fe19994bd2c569c1f3dfedab1d5164fc461f
jbachurski <kbachurski@gmail.com>
2023-03-17 17:23:56 +0000

    Fix test warnings due to pending const change

--- tests/test_constructors.py
+++ tests/test_constructors.py
@@ -7,7 +7,7 @@ from spox._type_system import Tensor
 
 def test_explicit_unspecified_optional(op, onnx_helper):
     (x,) = arguments(x=Tensor(numpy.float32, (None,)))
-    r = op.clip(x, min=None, max=op.const(0.0))
+    r = op.clip(x, min=None, max=op.constant(value_float=0.0))
     graph = results(r=r)
     onnx_helper.assert_close(
         onnx_helper.run(graph, "r", x=numpy.array([-1, 1, 2], numpy.float32)),
@@ -17,8 +17,8 @@ def test_explicit_unspecified_optional(op, onnx_helper):
 
 def test_unspecified_optional(op, onnx_helper):
     (x,) = arguments(x=Tensor(numpy.float32, (None,)))
-    r = op.clip(x, max=op.const(1.0))
-    r = op.clip(r, min=op.const(-1.0))
+    r = op.clip(x, max=op.constant(value_float=1.0))
+    r = op.clip(r, min=op.constant(value_float=-1.0))
     graph = results(r=r)
     onnx_helper.assert_close(
         onnx_helper.run(graph, "r", x=numpy.array([-3, -1, 1, 2], numpy.float32)),
