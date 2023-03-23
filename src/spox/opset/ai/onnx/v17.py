5226784852351e7a1f16a4d8eaef3bf4508eb6ca
jbachurski <kbachurski@gmail.com>
2023-03-23 11:34:17 +0000

    Run pre-commit hooks

--- src/spox/opset/ai/onnx/v17.py
+++ src/spox/opset/ai/onnx/v17.py
@@ -13147,8 +13147,8 @@ def scatter(
     """
     warnings.warn(
         "Scatter is a deprecated operator and its constructor should not be used. "
-    "Building will raise an error. "
-    "Deprecated constructors will be removed in Spox 0.7.0.",
+        "Building will raise an error. "
+        "Deprecated constructors will be removed in Spox 0.7.0.",
         DeprecationWarning,
         stacklevel=2,
     )
@@ -15494,8 +15494,8 @@ def upsample(
     """
     warnings.warn(
         "Upsample is a deprecated operator and its constructor should not be used. "
-    "Building will raise an error. "
-    "Deprecated constructors will be removed in Spox 0.7.0.",
+        "Building will raise an error. "
+        "Deprecated constructors will be removed in Spox 0.7.0.",
         DeprecationWarning,
         stacklevel=2,
     )
