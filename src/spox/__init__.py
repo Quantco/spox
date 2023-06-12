f5cbf62d6c8dea126570334ddbe60cf79fe3c14d
jbachurski <kbachurski@gmail.com>
2023-06-13 00:38:41 +0100

    Remove outdated patches

--- src/spox/__init__.py
+++ src/spox/__init__.py
@@ -1,23 +1,8 @@
-import onnx.helper as _helper  # noqa
 import pkg_resources
 
-# Apply fix for https://github.com/onnx/onnxmltools/issues/575 if we have onnxmltools
-try:
-    _make_tensor = _helper.make_tensor
-    import onnxmltools.proto as _omt_proto  # noqa
-
-    _helper.make_tensor = _make_tensor
-except ModuleNotFoundError:
-    pass
-
-from spox import _patch_ref_impl
 from spox._public import argument, build, inline
 from spox._type_system import Optional, Sequence, Tensor, Type
 from spox._var import Var
-
-# The onnx reference implementation has some known limitations which
-# we patch globally.
-_patch_ref_impl.patch_reference_implementations()
 
 __all__ = [
     "Var",
