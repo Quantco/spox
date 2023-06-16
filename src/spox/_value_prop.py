c6e0b6c3d0480b3bba406786b4fd75d41271adc9
Atanas Dimitrov <atanas.dimitrov@quantco.com>
2023-06-16 11:31:19 +0200

    Add additional handling for value-inference type-aliasing removal

--- src/spox/_value_prop.py
+++ src/spox/_value_prop.py
@@ -51,6 +51,10 @@ class PropValue:
     value: PropValueType
 
     def __post_init__(self):
+        # Fix dtype aliasing
+        if isinstance(self.value, numpy.ndarray) and numpy.issubdtype(self.value.dtype, numpy.number):
+            object.__setattr__(self, 'value', self.value.astype(numpy.dtype(self.value.dtype.name)))
+
         if VALUE_PROP_STRICT_CHECK and not self.check():
             raise ValueError(
                 f"Attempt to construct PropValue of {self.value}, "
@@ -111,7 +115,6 @@ class PropValue:
             return cls(typ, [cls.from_ort_value(elem_type, elem) for elem in value])
         elif isinstance(value, numpy.ndarray):  # Tensor
             # Normalise the dtype in case we got an alias (like longlong)
-            value = value.astype(numpy.dtype(value.dtype.name))
             if value.dtype == numpy.dtype(object):
                 value = value.astype(str)
             return cls(typ, value)
