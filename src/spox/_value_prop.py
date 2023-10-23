d62011072e97ab93aa495702fae79ebb37a08cfe
Aditya Goel <agoel4512@gmail.com>
2023-10-23 12:15:55 +0100

    Fix value prop failure

--- src/spox/_value_prop.py
+++ src/spox/_value_prop.py
@@ -1,4 +1,5 @@
 import enum
+import logging
 import warnings
 from dataclasses import dataclass
 from typing import Dict, List, Union
@@ -155,8 +156,12 @@ def _run_reference_implementation(
     try:
         session = onnx.reference.ReferenceEvaluator(model)
         output_feed = dict(zip(session.output_names, session.run(None, input_feed)))
-    except NotImplementedError:
+    except Exception as e:
         # Give up on value propagation if an implementation is missing.
+        logging.debug(
+            f"Value propagation in {model} on the ONNX reference implementation failed with - "
+            f"{type(e).__name__}: {e}"
+        )
         return {}
     return output_feed
 
@@ -173,7 +178,11 @@ def _run_onnxruntime(
         session = onnxruntime.InferenceSession(model.SerializeToString(), options)
         output_names = [output.name for output in session.get_outputs()]
         output_feed = dict(zip(output_names, session.run(None, input_feed)))
-    except onnxruntime.capi.onnxruntime_pybind11_state.Fail:
+    except (onnxruntime.capi.onnxruntime_pybind11_state.Fail, Exception) as e:
+        logging.debug(
+            f"Value propagation in {model} on the ONNX reference implementation failed with - "
+            f"{type(e).__name__}: {e}"
+        )
         return {}
     return output_feed
 
