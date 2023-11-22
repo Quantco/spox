52e7eb1681de82f0c5961a2b82d6db1b78f49780
Christian Bourjau <christian.bourjau@quantco.com>
2023-11-22 18:40:27 +0100

    Remove parametrization

--- tests/type_inference/test_compress.py
+++ tests/type_inference/test_compress.py
@@ -1,9 +1,6 @@
 import pytest
 
-import spox.opset.ai.onnx.v17 as op17
-import spox.opset.ai.onnx.v18 as op18
-import spox.opset.ai.onnx.v19 as op19
-import spox.opset.ai.onnx.v20 as op20
+import spox.opset.ai.onnx.v17 as op
 from spox._graph import arguments
 from spox._standard import InferenceError
 from spox._type_system import Tensor
@@ -16,8 +13,7 @@ def test_compress_inference():
     assert op.compress(x, y, axis=1).unwrap_tensor() == Tensor(float, ("N", None))
 
 
-@pytest.mark.parametrize("op", [op17, op18, op19, op20])
-def test_compress_inference_checks_bool_cond(op):
+def test_compress_inference_checks_bool_cond():
     (x,) = arguments(x=Tensor(float, ("N", "M")))
     with pytest.raises(InferenceError):
         op.compress(x, op.const(123)).unwrap_tensor()
