65a2dfc601c0d9b0db3f645fe7bccb3c614f8769
Christian Bourjau <christian.bourjau@quantco.com>
2025-12-31 12:30:23 +0100

    Review comment

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -1,7 +1,6 @@
 # Copyright (c) QuantCo 2023-2025
 # SPDX-License-Identifier: BSD-3-Clause
 
-import ml_dtypes
 import numpy as np
 import pytest
 
@@ -76,33 +75,6 @@ def test_bad_tensor_type():
             Tensor(typ)
 
 
-@pytest.mark.parametrize(
-    "dtype",
-    [
-        np.int8,
-        np.int16,
-        np.int32,
-        np.int64,
-        np.uint8,
-        np.uint16,
-        np.uint32,
-        np.uint64,
-        np.float16,
-        np.float32,
-        np.float64,
-        bool,
-        np.str_,
-        ml_dtypes.bfloat16,
-        ml_dtypes.float8_e4m3fn,
-        ml_dtypes.float8_e4m3fnuz,
-        ml_dtypes.float8_e5m2,
-        ml_dtypes.float8_e5m2fnuz,
-        ml_dtypes.uint4,
-        ml_dtypes.int4,
-        ml_dtypes.float4_e2m1fn,
-        ml_dtypes.float8_e8m0fnu,
-    ],
-)
 def test_supported_dtypes(dtype):
     # Check construction, serializing and deserializing of all supported data types
     tensor = Tensor(dtype)
